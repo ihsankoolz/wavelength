2dabe37a25204b9b240ab91cfa263aafc4d8e0ca
ihsankoolz <ihsanalfian008@gmail.com>
2025-10-05 20:57:41 +0800

    Initial project setup with Firebase configuration

---
+++ src/services/firebase.js
@@ -0,0 +1,17 @@
+    // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBkEIQdyds3kdAL3Zzn9rgbyLuRcpRsqDw",
+    authDomain: "wavelength-adde0.firebaseapp.com",
+    projectId: "wavelength-adde0",
+    storageBucket: "wavelength-adde0.firebasestorage.app",
+    messagingSenderId: "912354011753",
+    appId: "1:912354011753:web:7af3427a59eec781c810f8"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
